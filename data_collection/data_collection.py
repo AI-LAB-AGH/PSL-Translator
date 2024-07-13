f0cb65e5e4623c8b736018a27a016933fbb07cf4
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-07-13 12:23:25 +0200

    Added count_samples to data_collection

--- data_collection/data_collection.py
+++ data_collection/data_collection.py
@@ -4,11 +4,8 @@ import mediapipe as mp
 import keyboard
 
 
-def create_directory(name):
-    if not os.path.exists(os.path.join("data_updated/", action)):
-        os.makedirs(os.path.join("data_updated/", action))
-
-    sequence_list = os.listdir(os.path.join("data_updated/", action))
-    sequences_no = len(sequence_list)
-
-    os.makedirs(os.path.join("data_updated/", action, str(sequences_no)), exist_ok=True)
+def count_samples() -> int:
+    train_dirs = os.listdir(os.path.join("../data/landmarks/train/"))
+    test_dirs = os.listdir(os.path.join("../data/landmarks/test"))
+    total_dirs = len(train_dirs) + len(test_dirs)
+    return total_dirs
