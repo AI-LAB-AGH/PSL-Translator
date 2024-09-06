533d3e76718af77bbd0f404fa587475f24d9efe2
McOwska <zuza.m.makowska@gmail.com>
2024-09-06 21:01:39 +0200

    comments

--- models/model_LSTM_transformer.py
+++ models/model_LSTM_transformer.py
@@ -8,30 +8,28 @@ class LSTMTransformerModel(nn.Module):
         self.num_layers = num_layers
         self.attention_dim = attention_dim
 
-        # LSTM layers (pięć osobnych LSTM dla różnych części ciała, jak w oryginale)
         self.l = nn.LSTM(21 * 2, hidden_size, num_layers)
         self.r = nn.LSTM(21 * 2, hidden_size, num_layers)
         self.f2l = nn.LSTM(21 * 2, hidden_size, num_layers)
         self.f2r = nn.LSTM(21 * 2, hidden_size, num_layers)
         self.h2h = nn.LSTM(21 * 2, hidden_size, num_layers)
         
-        # Warstwa scalająca dane z pięciu LSTM, redukująca wymiar do ukrytego wymiaru Attention
         self.fuse = nn.Linear(5 * hidden_size, hidden_size)
 
-        # Mechanizm Attention (obliczanie uwagi nad wyjściem z LSTM)
-        self.attention = nn.Linear(hidden_size, attention_dim)  # Przekształcenie do przestrzeni uwagi
-        self.attention_score = nn.Linear(attention_dim, 1)  # Uzyskanie skalarnej wartości uwagi
+        self.attention = nn.Linear(hidden_size, attention_dim)
+        self.attention_score = nn.Linear(attention_dim, 1)
         
-        # Warstwa w pełni połączona (output)
         self.fc = nn.Linear(hidden_size, num_classes)
 
-        # Inicjalizacja stanów ukrytych i komórkowych LSTM
         self.h_l = self.c_l = self.h_r = self.c_r = self.h_f2r = self.c_f2l = self.h_h2h = self.c_h2h = None
 
     def initialize_cell_and_hidden_state(self) -> None:
-        """
-        Inicjalizacja stanu ukrytego i komórkowego dla wszystkich warstw LSTM.
-        Wywoływana raz przed każdym nowym treningiem lub inferencją.
+         """
+        Initializes the cell state and hidden state for both hand modules with zeros.
+        
+        Should be called once before passing in each training sample, and once before
+        starting real-time inference.
+        
         """
         self.h_l = torch.zeros([self.num_layers, 1, self.hidden_size])
         self.c_l = torch.zeros([self.num_layers, 1, self.hidden_size])
@@ -49,9 +47,6 @@ class LSTMTransformerModel(nn.Module):
         self.c_h2h = torch.zeros([self.num_layers, 1, self.hidden_size])
 
     def prepare_input(self, x: torch.tensor):
-        """
-        Funkcja przetwarzająca dane wejściowe - standaryzacja wymiarów i skalowanie poszczególnych danych
-        """
         x = x.float()
         x = x.unsqueeze(1)
 
@@ -91,54 +86,44 @@ class LSTMTransformerModel(nn.Module):
             hand2hand[:, 0] /= w_body
             hand2hand[:, 1] /= h_body
 
-        # Concatenacja wszystkich elementów i odpowiednie przekształcenie wymiarów
         x = torch.concat([left, right, face2left, face2right, hand2hand])
         x = x.view(1, 1, (21 + 21 + 21 + 21 + 21) * 2)
         
         return x
 
     def apply_attention(self, lstm_out):
-        """
-        Funkcja aplikująca mechanizm uwagi na wyjściu z LSTM.
-        """
-        attention_weights = torch.tanh(self.attention(lstm_out))  # Obliczanie "ciężarów" uwagi
-        attention_scores = self.attention_score(attention_weights)  # Obliczanie skalarnej wartości dla uwagi
-        attention_scores = torch.softmax(attention_scores, dim=1)  # Normalizacja przez softmax
-        
-        # Ważenie wyjść LSTM przez obliczone wartości uwagi
+        attention_weights = torch.tanh(self.attention(lstm_out)) 
+        attention_scores = self.attention_score(attention_weights) 
+        attention_scores = torch.softmax(attention_scores, dim=1) 
+
         context_vector = torch.sum(attention_scores * lstm_out, dim=1)
         
         return context_vector
 
     def forward(self, x: torch.tensor) -> torch.tensor:
         """
-        Główna funkcja forward, przetwarzająca dane przez LSTM, Attention, a następnie dająca wynik klasyfikacji.
+        Forwards a single frame of landmarks through the network. 
+
         """
         x = self.prepare_input(x)
         x = torch.permute(x, (1, 0, 2))
 
-        # Przejście przez poszczególne LSTM
         l, (self.h_l, self.c_l) = self.l(x[:, :, :21*2], (self.h_l, self.c_l))
         r, (self.h_r, self.c_r) = self.r(x[:, :, 21*2:42*2], (self.h_r, self.c_r))
         f2l, (self.h_f2l, self.c_f2l) = self.f2l(x[:, :, 42*2:63*2], (self.h_f2l, self.c_f2l))
         f2r, (self.h_f2r, self.c_f2r) = self.f2r(x[:, :, 63*2:84*2], (self.h_f2r, self.c_f2r))
         h2h, (self.h_h2h, self.c_h2h) = self.h2h(x[:, :, 84*2:], (self.h_h2h, self.c_h2h))
 
-        # Pobranie ostatnich kroków czasowych z każdego LSTM
         l = l[-1, :, :]
         r = r[-1, :, :]
         f2l = f2l[-1, :, :]
         f2r = f2r[-1, :, :]
         h2h = h2h[-1, :, :]
 
-        # Concatenacja wyników z różnych LSTM
         out = torch.concat([l, r, f2l, f2r, h2h], dim=1)
 
-        # Przejście przez warstwę fuzji (łączenie informacji z pięciu strumieni)
         out = self.fuse(out)
 
-        # Zastosowanie mechanizmu uwagi na wyjściu
         context_vector = self.apply_attention(out.unsqueeze(1))
 
-        # Ostateczna warstwa w pełni połączona
         return self.fc(context_vector)
