09cd97502d8eadeaac9d5ef2e005e8a73dab663e
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-11-01 19:15:30 +0100

    Added GRU model

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -2,5 +2,11 @@ import wandb
 
 
 sweep_configuration = {
-    'method': 'bayes'
+    'method': 'bayes',
+    'metric': {'name': 'acc_mean', 'goal': 'maximize'},
+    'parameters': {
+        'model': {'values': ['LSTM', 'GRU']},
+        'hidden_dim': {'values': [64, 128, 192, 256, 320, 384, 448, 512]},
+        'learning_rate': {'values': [0.0001, 0.001, 0.01, 0.1]}
+    }
 }
