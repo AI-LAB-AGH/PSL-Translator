10d2aaf3b5d1307305668d09b9974e95f8984a47
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2024-11-06 08:45:10 +0200

    .

--- translation/translator.py
+++ translation/translator.py
@@ -4,14 +4,18 @@ import definitions
 
 
 class Translator:
-    def __init__(self, model="translation/models/pretrained_models/allegro/plt5-large/checkpoint",
+    def __init__(self, model="translation/models/pretrained_models/allegro/plt5-large-more-data/checkpoint",
                  tokenizer="allegro/plt5-large"):
         self.model = transformers.AutoModelForSeq2SeqLM.from_pretrained(
             os.path.join(definitions.ROOT_DIR,
                          model)).to("cpu")
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer)
+        self.model.eval()
 
     def translate(self, sentence):
-        sentence = " ".join(sentence)
+        if type(sentence) is list:
+            sentence = " ".join(sentence)
+        sentence = f"Przetłumacz zdanie z polskiego języka migowego na polski: {sentence} cel: "
         outputs = self.model.generate(self.tokenizer(sentence, return_tensors="pt")['input_ids'].to("cpu"))
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
